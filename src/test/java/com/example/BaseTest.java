d214d619cb8e3c91429cfdaf47e9af4e293a84e4
Atishay sharma <atishaysharma640@gmail.com>
2019-09-11 10:04:38 +0530

    Created test which is tested by saucelabs.

--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -4,7 +4,11 @@ import com.example.utils.ApplicationProperties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
+
+import java.net.URL;
 
 public class BaseTest {
 
@@ -15,12 +19,27 @@ public class BaseTest {
         System.out.println("Start");
     }
 
+//    @BeforeTest
+//    public void beforeTest() {
+//        System.out.println("before test");
+//        String driverPath = ApplicationProperties.INSTANCE.getDriverPath();
+//        System.setProperty("webdriver.chrome.driver", driverPath);
+//        driver = new ChromeDriver();
+//    }
+
     @BeforeTest
-    public void beforeTest() {
-        System.out.println("before test");
-        String driverPath = ApplicationProperties.INSTANCE.getDriverPath();
-        System.setProperty("webdriver.chrome.driver", driverPath);
-        driver = new ChromeDriver();
+    public void runWithSauceLabs() throws Exception{
+        String sauceUserName = "atishaysharma";
+        String sauceAccessKey = "a747e449-d764-4de7-a177-d7f7dc5dae72";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("username", sauceUserName);
+        capabilities.setCapability("accessKey", sauceAccessKey);
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("platform", "Windows 10");
+        capabilities.setCapability("version", "59.0");
+        capabilities.setCapability("name", "My_first_saucelabs_test");
+        driver = new RemoteWebDriver(new URL("http://ondemand.saucelabs.com:80/wd/hub"), capabilities);
     }
 
     @AfterTest
